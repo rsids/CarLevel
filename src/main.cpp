0395a216f1cd5bc4cf0ecf61fc5d1e4da2b6f6b3
Ids Klijnsma <werk@idsklijnsma.nl>
2021-11-05 08:42:55 +0100

    Moved to seperate classes

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,26 @@
 #include <BLEServer.h>
 #include <BLE2902.h>
 
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
 // BT CLASSIC
 // #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 // #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 // #endif
 
-// BluetoothSerial SerialBT;
-
 #define SERVICE_UUID "ac159216-381a-11ec-8d3d-0242ac130003"
 #define CHARACTERISTIC_UUID "b08cd61a-381a-11ec-8d3d-0242ac130003"
 #define DESCRIPTOR_UUID "72ccbe10-cfd1-4d31-9d67-50b76db62e95"
+
 //#define CHARACTERISTIC_UUID 0x2763
 
 BLEServer *pServer;
 BLEService *pService;
 BLECharacteristic *pCharacteristic;
+
+Adafruit_MPU6050 mpu;
 
 bool deviceConnected = false;
 
@@ -36,6 +41,85 @@ class LevelBLEServerCallbacks : public BLEServerCallbacks
     BLEDevice::startAdvertising();
   };
 };
+
+void setup_mpu()
+{
+  if (!mpu.begin())
+  {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1)
+    {
+      delay(10);
+    }
+  }
+  Wire.begin();
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange())
+  {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange())
+  {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth())
+  {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+}
 
 int xPin = 36;
 int yPin = 33;
@@ -68,25 +152,59 @@ void setup()
   pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
+
+  setup_mpu();
+
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
+// void read_sensors()
+// {
+//   sensors_event_t a, g, temp;
+//   mpu.getEvent(&a, &g, &temp);
+
+//   /* Print out the values */
+//   Serial.print("Acceleration X: ");
+//   Serial.print(a.acceleration.x);
+//   Serial.print(", Y: ");
+//   Serial.print(a.acceleration.y);
+//   Serial.print(", Z: ");
+//   Serial.print(a.acceleration.z);
+//   Serial.println(" m/s^2");
+
+//   Serial.print("Rotation X: ");
+//   Serial.print(g.gyro.x);
+//   Serial.print(", Y: ");
+//   Serial.print(g.gyro.y);
+//   Serial.print(", Z: ");
+//   Serial.print(g.gyro.z);
+//   Serial.println(" rad/s");
+
+//   Serial.print("Temperature: ");
+//   Serial.print(temp.temperature);
+//   Serial.println(" degC");
+
+//   Serial.println("");
+//   delay(500);
+// }
+
 void loop()
 {
-  digitalWrite(2, LOW);
-  if (deviceConnected)
-  {
-    digitalWrite(2, HIGH);
-    char buffer2[9];
-    // put your main code here, to run repeatedly:
-    float valX = ((float)analogRead(xPin) * scale) - 180;
-    float valY = ((float)analogRead(yPin) * scale) - 180;
-    sprintf(buffer2, "%0.0f;%0.0f", valX, valY);
-    Serial.println(buffer2);
+  // read_sensors();
+  // digitalWrite(2, LOW);
+  // if (deviceConnected)
+  // {
+  //   digitalWrite(2, HIGH);
+  //   char buffer2[9];
+  //   // put your main code here, to run repeatedly:
+  //   float valX = ((float)analogRead(xPin) * scale) - 180;
+  //   float valY = ((float)analogRead(yPin) * scale) - 180;
+  //   sprintf(buffer2, "%0.0f;%0.0f", valX, valY);
+  //   Serial.println(buffer2);
 
-    pCharacteristic->setValue(buffer2);
-    pCharacteristic->notify();
+  //   pCharacteristic->setValue(buffer2);
+  //   pCharacteristic->notify();
 
-    delay(500); // stop the program for some time
-  }
+  //   delay(500); // stop the program for some time
+  // }
 }
